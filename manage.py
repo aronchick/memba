2913d11f73ea193d8d18bb11db7ca3943a80449a
David Aronchick <aronchick@gmail.com>
2021-12-21 11:35:13 -0800

    figured out python path issues

--- manage.py
+++ manage.py
@@ -3,7 +3,11 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+    # sys.path.append("/Users/daaronch/code/memba/site")
+    sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/site")
+    print(sys.path)
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
     from django.core.management import execute_from_command_line
 
